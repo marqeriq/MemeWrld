cf733796b0347e1141423d1d0d296152218dfde2
kanto riqy <37961115-eriqmainamic@users.noreply.replit.com>
2024-11-22 17:34:08 +0000

    index page

--- app.py
+++ app.py
@@ -1,11 +1,11 @@
-from flask import Flask
+from flask import Flask, render_template
 
 app = Flask(__name__)
 
 
 @app.route("/")
 def hello():
-  return "Hello, MemeWrld!"
+  return render_template("index.html")
 
 
 print(__name__)
